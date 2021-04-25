81277c80b18f0e5bbf3d7c4c532019180e881f7a
HonusDaniel <danielhonus@outlook.cz>
2021-04-25 10:57:19 +0200

    more

--- maze.c
+++ maze.c
@@ -1,78 +1,70 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "maze.h"
 #include <assert.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // https://stackoverflow.com/questions/60656309/how-could-i-optimize-my-breadth-search-algorithm-for-solving-bigger-mazes
 
-int solve(char* filename)
-{
-  FILE* maze;
+int solve(char *filename) {
+  FILE *maze;
 
   // if can't load file, exit
-  if (!(maze = fopen(filename, "r"))){
+  if (!(maze = fopen(filename, "r"))) {
     printf("could not find '%s' file with maze, exiting...\n", filename);
     assert(!fclose(maze));
     exit(EXIT_FAILURE);
   }
 
-  //if okay continue to solve
-  FILE* output = fopen("output", "w+");
+  // if okay continue to solve
+  FILE *output = fopen("output", "w+");
 
   int width, height;
   fscanf(maze, "%d %d", &width, &height);
-  
+
   int maze_array[width][height];
   bool visited[width][height];
 
-  //read and print
-  for (int i = 0; i < height; i++)
-  {
-    for(int j = 0; j < width; j++)
-    {
+  // read and print
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
       fscanf(maze, "%1d", &maze_array[i][j]);
       visited[i][j] = false;
     }
   }
-
-  
 
   assert(!fclose(maze));
   assert(!fclose(output));
   return 0;
 }
 
-
-//optional function to print a maze file using special characters
-void printmaze(char* filename){
+// optional function to print a maze file using special characters
+void printmaze(char *filename) {
   int width, height = 0;
-  FILE* maze;
+  FILE *maze;
 
   // if can't load file, exit
-  if (!(maze = fopen(filename, "r"))){
+  if (!(maze = fopen(filename, "r"))) {
     printf("could not find '%s' file with maze, exiting...\n", filename);
     assert(!fclose(maze));
     exit(EXIT_FAILURE);
   }
 
-
-  //get size of maze
+  // get size of maze
   fscanf(maze, "%d %d", &width, &height);
   int maze_array[width][height];
 
-  //read and print
-  for (int i = 0; i < height; i++)
-  {
-    for(int j = 0; j < width; j++)
-    {
+  // read and print
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
       fscanf(maze, "%1d", &maze_array[i][j]);
       if (maze_array[i][j] == 1)
         printf("#");
-      else if (maze_array[i][j] == 2 || maze_array[i][j] == 3) 
-          printf("X");
-        else printf(" ");
+      else if (maze_array[i][j] == 2 || maze_array[i][j] == 3)
+        printf("X");
+      else
+        printf(" ");
     }
     printf("\n");
   }
